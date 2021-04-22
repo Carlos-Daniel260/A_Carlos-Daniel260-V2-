9b0473eea0be22dceb7e32bb8173f9da13e094c9
Edwar3D <58242017+Edwar3D@users.noreply.github.com>
2021-04-21 23:08:54 -0500

    Compra y detalle compra termindo

--- src/main/java/org/springframework/samples/petclinic/ventaproductos/VentaProductoAPIController.java
+++ src/main/java/org/springframework/samples/petclinic/ventaproductos/VentaProductoAPIController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.samples.petclinic.owner.OwnerRepository;
-import org.springframework.samples.petclinic.user.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,20 +15,22 @@ import java.util.Iterator;
 class VentaProductoAPIController {
 
     private final VentaRepository ventas;
+    private final VentaDetalleRepository ventaDetalleRepository;
 
     @Autowired
     private OwnerRepository owners;
 
 
-    public VentaProductoAPIController(VentaRepository ventas){
+    public VentaProductoAPIController(VentaRepository ventas, VentaDetalleRepository ventaDetalleRepository){
         this.ventas = ventas;
+        this.ventaDetalleRepository = ventaDetalleRepository;
     }
 
-    @GetMapping({ "/API/comprasJSON/{idowner}" })
-    public @ResponseBody VentaProductoJSON showResourcesVetListJSON( @PathVariable("idowner") int idowner) {
+    @GetMapping({ "/API/compras/{idowner}" })
+    public @ResponseBody VentaProductoJSON showResourcesVentasListJSON( @PathVariable("idowner") int idowner) {
         VentaProductoJSON misCompras = new VentaProductoJSON();
         Owner owner_temp = this.owners.findOwnerByUserId(idowner);
-
+        System.out.println(owner_temp);
         if(owner_temp != null){
             Collection<Venta> allCompras = this.ventas.ventasOwner(owner_temp);
             Iterator iterator = allCompras.iterator();
@@ -40,7 +41,24 @@ class VentaProductoAPIController {
             return misCompras;
         }else
             return null;
+    }
 
+    @GetMapping({ "/API/compraDetalle/{idowner}" })
+    public @ResponseBody
+    CompraDetalleJSON showResourcesCompraDetalleJSON(@PathVariable("idowner") int idCompra) {
+        CompraDetalleJSON miCompra = new CompraDetalleJSON();
+        Venta venta_temp = this.ventas.findById(idCompra);
+        System.out.println(venta_temp);
+        if(venta_temp != null){
+            Collection<VentaDetalle> detallesCompra = venta_temp.getVentadetalle();
+            Iterator iterator = detallesCompra.iterator();
+            while(iterator.hasNext()) {
+                VentaDetalle venta = (VentaDetalle) iterator.next();
+                miCompra.getVentasList().add( new CompraDetalle(venta.getId(), venta.getProduct().getName(), venta.getCantidad(), venta.getPrecio(), venta.getSubtotal()) );
+            }
+            return miCompra;
+        }else
+            return null;
     }
 
 }
